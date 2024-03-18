91171fee4be2d26d5d68f27a72ca4d977a66bb52
Salim Ferez Mendez <123603908+salfermen@users.noreply.github.com>
2024-03-17 22:30:53 -0500

    Create Cuenta.java

---
+++ proyectoIIISemestreProg/src/model/Cuenta.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Cuenta {
+
+	public Cuenta(String id, double saldo) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void retirar(double cantidad) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void depositar(double cantidad) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getSaldo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
